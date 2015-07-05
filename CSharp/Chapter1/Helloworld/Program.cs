6b4c5071c1f4556156bf3a763649258b0b9ab14b
vetriselvan <vetri@aktrea.com>
2015-07-05 16:28:00 +0530

    Soution files added

--- CSharp/Chapter1/Helloworld/Program.cs
+++ CSharp/Chapter1/Helloworld/Program.cs
@@ -10,7 +10,10 @@ namespace Helloworld
     {
         static void Main(string[] args)
         {
+            // Display a simple message to the user.    
+            Console.WriteLine("***** My First C# App *****"); 
             Console.WriteLine("Hello World");
+            // Wait for Enter key to be pressed before shutting down. 
             Console.ReadLine();
         }
     }
