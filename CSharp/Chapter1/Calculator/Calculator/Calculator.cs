8ba4d2f5c03dae04d427f2ebb7fc05014e6f5a30
vetriselvan <vetri@aktrea.com>
2015-07-05 14:41:13 +0530

    Initial Commit

---
+++ CSharp/Chapter1/Calculator/Calculator/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    /// <summary>
+    ///Contains functions for Arithmetic Operations
+    /// </summary>
+    public class Calculator
+    {
+
+        //Add two numbers
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        //Subtract two numbers
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        //Multiply two numbers
+        public int Mul(int x, int y)
+        {
+            return x * y;
+
+        }
+        //Divide two numbers
+        public double Div(double x, double y)
+        {
+            return x / y;
+        }
+        //Modulous two numbers
+        public double Modulo(double x, double y)
+        {
+            return x % y;
+        }
+    }
+}
---
+++ CSharp/Chapter1/Calculator/Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    //Application Entry point
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            int add = calc.Add(10, 34);
+            Console.WriteLine("10+34 is {0}.", add);
+            int sub = calc.Sub(34, 10);
+            Console.WriteLine("34-10 is {0}.", sub);
+            int mul = calc.Mul(30, 40);
+            Console.WriteLine("30*40 is {0}.", mul);
+            double div = calc.Div(500, 6);
+            Console.WriteLine("500/6 is {0}.", div);
+            double modulo = calc.Modulo(56, 9);
+            Console.WriteLine("56*9 is {0}.", modulo);
+            Console.ReadLine();
+
+        }
+    }
+}
